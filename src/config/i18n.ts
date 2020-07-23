643c6e99e567e8be75ee6f8873aff7284a871aba
Nando Vieira <me@fnando.com>
2020-07-23 11:39:08 -0700

    Add Spanish translations.

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,9 +1,11 @@
 import { Catalog, setupI18n } from "@lingui/core";
 
 import en from "locales/en/messages";
+import es from "locales/es/messages";
 
 const catalogs: { [key: string]: Catalog } = {
   en,
+  es,
 };
 
 export const i18n = setupI18n({
