3a269587576cd75122d44c695a2d837cb1f2fa10
Nando Vieira <me@fnando.com>
2022-04-27 12:38:11 -0700

    Load Ukrainian translations. (#79)

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -2,10 +2,12 @@ import { Catalog, setupI18n } from "@lingui/core";
 
 import en from "locales/en/messages";
 import es from "locales/es/messages";
+import uk from "locales/uk/messages";
 
 const catalogs: { [key: string]: Catalog } = {
   en,
   es,
+  uk,
 };
 
 export const i18n = setupI18n({
