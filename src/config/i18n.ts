0a6f9edd2e1ab55b22e80986a074666e252a4b38
Nando Vieira <me@fnando.com>
2022-05-02 12:33:05 -0700

    Use i18n._ instead. (#84)

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -16,6 +16,8 @@ export const i18n = setupI18n({
 });
 
 // Strings that need to be translated because they're being used on index.html
-i18n.t`Please update Android WebView to start the session.`;
-i18n.t`Please update Google Chrome to start the session.`;
-i18n.t`Your device’s browser is not compatible. Please update your browser and Vibrant to the latest version.`;
+i18n._(`Please update Android WebView to start the session.`);
+i18n._(`Please update Google Chrome to start the session.`);
+i18n._(
+  `Your device’s browser is not compatible. Please update your browser and Vibrant to the latest version.`,
+);
