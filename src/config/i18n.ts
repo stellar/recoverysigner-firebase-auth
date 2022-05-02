b8c46ea8a539e8d3d0cfa745909965f1858d44e1
Nando Vieira <me@fnando.com>
2022-05-02 12:09:25 -0700

    Extract strings from html file. (#82)
    Also improve and make sure we're always displaying something, falling
    back to `en-US`.

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -14,3 +14,8 @@ export const i18n = setupI18n({
   language: "en",
   catalogs,
 });
+
+// Strings that need to be translated because they're being used on index.html
+i18n.t`Please update Android WebView to start the session.`;
+i18n.t`Please update Google Chrome to start the session.`;
+i18n.t`Your device’s browser is not compatible. Please update your browser and Vibrant to the latest version.`;
